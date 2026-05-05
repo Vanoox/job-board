56fd4019bc1003473ca826bdd3109f6f802b4f66
Mikołaj Dyk <48884796+Vanoox@users.noreply.github.com>
2026-05-05 22:09:08 +0200

    update 1

---
+++ src/database.ts
@@ -0,0 +1,13 @@
+import { Pool } from "pg";
+import { Kysely, PostgresDialect } from "kysely";
+import type { DB } from "./db.d.ts";
+
+const dialect = new PostgresDialect({
+  pool: new Pool({
+    connectionString: process.env.DATABASE_URL,
+  }),
+});
+
+export const db = new Kysely<DB>({
+  dialect,
+});
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
-console.log("Hello, worldsssss!");
+import "./env.ts";
+import express, { type Request, type Response } from "express";
+import { db } from "./database.ts";
+import type { Company } from "./db.d.ts";
+import { sql } from "kysely";
+
+const app = express();
+const port = 3000;
+
+app.get("/", async (req, res) => {
+  const { rows: results } = await sql<Company[]>`select * from company`.execute(
+    db,
+  );
+  res.send(results);
+});
+
+app.use((err: Error, req: Request, res: Response, next: unknown) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
